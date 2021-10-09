70a48ba5abca42fa9a0ab381fb041a6501520a5e
@sebastian.zapata <sebastian.zapata@geniussportsmedia.com>
2021-10-08 20:15:27 -0500

    card list component and grid with css to separete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import "./App.css";
 import React, { Component } from "react";
 
+import { CardList } from "./components/card-list/card-list.component";
+
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      monsters :[]
-    }
+    this.state = {
+      monsters: [],
+    };
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response =>response.json())
-    .then(users => this.setState({monsters:users}))
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
-        }
+        <CardList name="Sebas">
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.key}>{monster.name} </h1>
+          ))}
+        </CardList>
       </div>
     );
   }
