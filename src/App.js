266c90d03f404721401dba2de3107da33e053899
@sebastian.zapata <sebastian.zapata@geniussportsmedia.com>
2021-10-11 17:31:31 -0500

    refactor to make reusable search box component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { Component } from "react";
 
 import { CardList } from "./components/card-list/card-list.component";
+import {SearchBox} from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -25,10 +26,10 @@ class App extends Component {
       )
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search monster"
-          onChange={(e) =>
+        
+        <SearchBox 
+          placeholder='search monster'
+          handleChange = {(e) =>
             this.setState({
               searchField: e.target.value
             })
